0a55f5af158dd693f911e099dd56e8666df74f40
Mike Morton <mikemort@microsoft.com>
2019-05-01 18:21:23 -0700

    Add project files.

---
+++ LogIt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LogIt
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int count = 0;
+            while (true)
+            {
+                count += 1;
+                Console.WriteLine("Hello World " + count);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
